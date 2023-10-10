20067d58d969fbf350ba30efcfba5f3139f08572
Cameron Cardinal <cameriancardinal@gmail.com>
2023-10-10 11:16:32 +0200

    Menu

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // let spanDeux = document.getElementById("span-2")
 let spanUn = document.querySelector("#span-1");
 let spanDeux = document.querySelector("#span-2");
+let hamburger = document.querySelector("#hamburger");
 // spanDeux.style.backgroundColor = "black";
 
 spanUn.addEventListener("click", function(){
     spanDeux.classList.toggle("rotate");
+}
+)
+
+hamburger.addEventListener("click", function(){
+    menuList.classList.toggle("flex");
 }
 )
