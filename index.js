3b9afa4391288d2e00546344b3ec23dc76171f17
Cameron Cardinal <cameriancardinal@gmail.com>
2023-10-10 13:37:10 +0200

    Corrections

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 // let spanDeux = document.getElementById("span-2")
 let spanUn = document.querySelector("#span-1");
 let spanDeux = document.querySelector("#span-2");
-let hamburger = document.querySelector("#hamburger");
+let hamburger = document.querySelector("#toggle-menu");
+let menuList = document.querySelector("#nav-list");
+let links = document.querySelectorAll("#nav-list > li > a");
 // spanDeux.style.backgroundColor = "black";
 
-spanUn.addEventListener("click", function(){
+spanUn.addEventListener("click", function () {
     spanDeux.classList.toggle("rotate");
 }
 )
 
-hamburger.addEventListener("click", function(){
+hamburger.addEventListener("click", function () {
     menuList.classList.toggle("flex");
-}
-)
+    hamburger.classList.toggle("close");
+    if (hamburger.classLisr.contains("close")) {
+        hamburger.textContent = "Close menu";
+    } else {
+        hamburger.textContent = "Open menu";
+    }
+});
+
+menuLisr.addEventListener("click", function () {
+    menuList.classList.toggle("flex");
+    hamburger.classList.toggle("close");
+    if (hamburger.classLisr.contains("close")) {
+        hamburger.textContent = "Close menu";
+    } else {
+        hamburger.textContent = "Open menu";
+    }
+})
