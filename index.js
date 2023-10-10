dce5569e62ba0ed9638a608621cfd6d95728befc
Cameron Cardinal <cameriancardinal@gmail.com>
2023-10-10 10:21:13 +0200

    Add Javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+// let spanDeux = document.getElementById("span-2")
+let spanUn = document.querySelector("#span-1");
+let spanDeux = document.querySelector("#span-2");
+// spanDeux.style.backgroundColor = "black";
+
+spanUn.addEventListener("click", function(){
+    spanDeux.classList.toggle("black");
+}
+)
